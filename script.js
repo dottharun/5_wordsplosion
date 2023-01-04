1e82e1674c5aa5c13820c9d739172499df89705a
dottharun <tharun242424@gmail.com>
2023-01-04 13:22:24 +0530

    js getting today secretword

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let secretWord = undefined;
+
+// function to use API to get the secret word of the day
+async function getSecretWord() {
+  const TODAY_WORD_URL = `https://words.dev-apis.com/word-of-the-day`;
+  const promiseResponse = await fetch(TODAY_WORD_URL);
+  const processedPromise = await promiseResponse.json();
+  secretWord = processedPromise.word.toUpperCase();
+}
+
+function init() {
+  console.log("boo");
+  getSecretWord();
+}
+
+init();
